20d359ed3f02423ec59e9726f3132e62a05f153a
Logan Boswell <loganstuartboswell@gmail.com>
2024-11-06 15:24:44 -0600

    Wrote launchfile to run Gazebo and downloaded models

--- diff_drive/setup.py
+++ diff_drive/setup.py
@@ -1,4 +1,15 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+def recursive_files(prefix, path):
+    """
+    Recurse over path returning a list of tuples suitable for use with setuptools data_files.
+    :param prefix: prefix path to prepend to the path
+    :param path: Path to directory to recurse. Path should not have a trailing '/'
+    :return: List of tuples. First element of each tuple is destination path, second element is a list of files to copy to that path
+    """
+    return [(str(Path(prefix)/subdir),
+             [str(file) for file in subdir.glob('*') if not file.is_dir()] ) for subdir in Path(path).glob('**')]
 
 package_name = 'diff_drive'
 
@@ -10,13 +21,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/worlds', ['worlds/ddrive.world.sdf'])
+        ('share/' + package_name + '/env-hooks', ['env-hooks/diff_drive.dsv']),
+        *recursive_files('share/' + package_name, "models"),
+        *recursive_files('share/' + package_name, "worlds"),
+        ('share/' + package_name + '/launch', ['launch/ddrive.launch.xml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Logan Boswell',
     maintainer_email='loganstuartboswell@gmail.com',
-    description='TODO: Package description',
+    description='A package for simulating a differential drive robot in Gazebo',
     license='GPLv3',
     tests_require=['pytest'],
     entry_points={
