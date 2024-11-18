6447be179263e821472d193dc93741abd957d003
Logan Boswell <loganstuartboswell@gmail.com>
2024-11-17 20:43:09 -0600

    Made changes to inertia properties and removed friction; fixed flip node

--- diff_drive/diff_drive/flip.py
+++ diff_drive/diff_drive/flip.py
@@ -2,28 +2,11 @@
 
 from enum import Enum, auto
 
-from geometry_msgs.msg import TransformStamped, Twist, Vector3
+from geometry_msgs.msg import Twist, Vector3
 
 import rclpy
 from rclpy.node import Node
 
-import tf2_ros
-from tf2_ros import TransformBroadcaster
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
-
-
-class State(Enum):
-    """
-    Current state of the system.
-
-    Determines what the main timer function should be doing on each iteration
-
-    """
-
-    NORMAL = auto(),
-    FLIPPED = auto(),
-    STOPPED = auto()
 
 
 class Flip(Node):
@@ -40,75 +23,22 @@ class Flip(Node):
         super().__init__('flip')
         self.tmr = self.create_timer(1/100, self.timer_callback)
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
-
-        self.tf_broadcaster = TransformBroadcaster(self, 10)
-        self.buffer = Buffer()
-        self.tf_listener = TransformListener(self.buffer, self)
-
-        self.state = State.STOPPED
-        self.vel = 6.5
-
-        odom_base_tf = TransformStamped()
-        odom_base_tf.header.stamp = self.get_clock().now().to_msg()
-        odom_base_tf.header.frame_id = 'odom'
-        odom_base_tf.child_frame_id = 'base_link'
-        odom_base_tf.transform.translation.x = 0.0
-        self.tf_broadcaster.sendTransform(odom_base_tf)
-
-        self.start_time = self.get_clock().now().to_msg()
+        self.vel = 2.0
+        self.count = 0
 
     def timer_callback(self):
         """
         Timer callback for controlling the turtle robot.
 
-        Publishes commands at a set frequency paramter (commands vary based on
-        robot state)
+        Publishes commands at a set frequency paramter 
 
         """
-        if self.state != State.STOPPED:
-            self.vel_pub.publish(
-                Twist(
-                    linear=Vector3(
-                        x=self.vel
-                    )
-                )
-            )
-            try:
-                tf_odom_base = self.buffer.lookup_transform('odom',
-                                                            'base_link',
-                                                            rclpy.time.Time())
-            except tf2_ros.LookupException:
-                pass
-            except tf2_ros.ConnectivityException:
-                pass
-            except tf2_ros.ExtrapolationException:
-                pass
+        if self.count > 300:
+            self.vel *= -1
+            self.count = 0
 
-            tf_odom_base = self.buffer.lookup_transform('odom',
-                                                        'base_link',
-                                                        rclpy.time.Time())
-            if tf_odom_base.transform.translation.x >= 3.0:
-                self.vel = -6.5
-            elif tf_odom_base.transform.translation.x <= -3.0:
-                self.vel = 6.5
-
-            odom_base_tf = TransformStamped()
-            odom_base_tf.header.stamp = self.get_clock().now().to_msg()
-            odom_base_tf.header.frame_id = 'odom'
-            odom_base_tf.child_frame_id = 'base_link'
-            odom_base_tf.transform.translation.x = \
-                tf_odom_base.transform.translation.x
-            self.tf_broadcaster.sendTransform(odom_base_tf)
-
-        odom_base_tf = TransformStamped()
-        odom_base_tf.header.stamp = self.get_clock().now().to_msg()
-        odom_base_tf.header.frame_id = 'odom'
-        odom_base_tf.child_frame_id = 'base_link'
-        odom_base_tf.transform.translation.x = 0.0
-        self.tf_broadcaster.sendTransform(odom_base_tf)
-
-        if self.get_clock().now().to_msg().sec - self.start_time.sec > 5:
-            self.state = State.NORMAL
+        self.vel_pub.publish(Twist(linear=Vector3(x=self.vel)))
+        self.count += 1
 
 
 def main(args=None):
